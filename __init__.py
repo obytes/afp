412af964feed6ec63e898dad15bada647bfea854
= <=>
2014-01-24 18:17:42 +0000

    fab command to list All Relational Data Service

--- __init__.py
+++ __init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
 from fabulous import *
+from s3 import *
+from rds import *
 from recipes.settings import *
---
+++ rds.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import boto
+from prettytable import PrettyTable as _pretty_table
+from fabric.api import *
+from fabric.colors import green as _green, yellow as _yellow, red as _red
+from global_conf import *
+from conf.parse_config import parse_ini
+import time
+
+def rds_list():
+    '''List All Relational Data Service'''
+    print(_green("Listing databases..."))
+    rds = boto.connect_rds(ec2_key, ec2_secret)
+    instances = rds.get_all_dbinstances()
+
+    x = _pretty_table(["RDS ID", "Create date", "Engine", "Status", "Port", "Address"])
+    for i in instances:
+        x.add_row([i.id, i.create_time, i.engine, i.status, i._port, i._address ])
+
+    print(_yellow(x))
+
+def rds_health():
+    '''Get RDS Health'''
+    rds = boto.connect_rds(ec2_key, ec2_secret)
+    print rds.get_all_events()
