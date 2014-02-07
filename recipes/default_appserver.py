778a8e0924fec9061dcc1ed90691e340b36b0d59
mo-mughrabi <mo.mughrabi@gmail.com>
2014-02-07 17:18:11 +0300

    update staging recipe to install full stack (rabbitmq and database)

--- recipes/default_appserver.py
+++ recipes/default_appserver.py
@@ -32,34 +32,34 @@ create_recipe_production = [
                                  "destination": "/home/%(SERVER_USERNAME)s/nginx.conf"},
      "message": "Configuring nginx"},
     {"action": "sudo", "params":
-     "mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old"},
+        "mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old"},
     {"action": "sudo", "params":
-     "mv /home/%(SERVER_USERNAME)s/nginx.conf /etc/nginx/nginx.conf"},
+        "mv /home/%(SERVER_USERNAME)s/nginx.conf /etc/nginx/nginx.conf"},
     {"action": "sudo", "params": "chown root:root /etc/nginx/nginx.conf"},
     {"action": "put_template", "params": {"template": "%(FABULOUS_PATH)s/recipes/templates/nginx-app-proxy",
                                           "destination": "/home/%(SERVER_USERNAME)s/%(PROJECT_NAME)s"}},
     {"action": "sudo", "params":
-     "rm -rf /etc/nginx/sites-enabled/default"},
+        "rm -rf /etc/nginx/sites-enabled/default"},
     {"action": "sudo",
      "params": "mv /home/%(SERVER_USERNAME)s/%(PROJECT_NAME)s /etc/nginx/sites-available/%(PROJECT_NAME)s"},
     {"action": "sudo",
      "params": "ln -s /etc/nginx/sites-available/%(PROJECT_NAME)s /etc/nginx/sites-enabled/%(PROJECT_NAME)s"},
     {"action": "sudo", "params":
-     "chown root:root /etc/nginx/sites-available/%(PROJECT_NAME)s"},
+        "chown root:root /etc/nginx/sites-available/%(PROJECT_NAME)s"},
     {"action": "sudo", "params": "/etc/init.d/nginx restart",
      "message": "Restarting nginx"},
 
     # virtualenvwrapper
     {"action": "sudo", "params":
-     "mkdir %(VIRTUALENV_DIR)s", "message": "Configuring virtualenvwrapper"},
+        "mkdir %(VIRTUALENV_DIR)s", "message": "Configuring virtualenvwrapper"},
     {"action": "sudo", "params":
-     "chown -R %(SERVER_USERNAME)s: %(VIRTUALENV_DIR)s"},
+        "chown -R %(SERVER_USERNAME)s: %(VIRTUALENV_DIR)s"},
     {"action": "run", "params":
-     "echo 'export WORKON_HOME=%(VIRTUALENV_DIR)s' >> /home/%(SERVER_USERNAME)s/.profile"},
+        "echo 'export WORKON_HOME=%(VIRTUALENV_DIR)s' >> /home/%(SERVER_USERNAME)s/.profile"},
     {"action": "run",
      "params": "echo 'source /usr/local/bin/virtualenvwrapper.sh' >> /home/%(SERVER_USERNAME)s/.profile"},
     {"action": "run", "params":
-     "source /home/%(SERVER_USERNAME)s/.profile"},
+        "source /home/%(SERVER_USERNAME)s/.profile"},
 
     # webapps alias
     {"action": "run", "params": """echo "alias webapps='cd %(APPS_DIR)s'" >> /home/%(SERVER_USERNAME)s/.profile""",
@@ -67,23 +67,23 @@ create_recipe_production = [
 
     # webapps dir
     {"action": "sudo", "params":
-     "mkdir %(APPS_DIR)s", "message": "Creating webapps directory"},
+        "mkdir %(APPS_DIR)s", "message": "Creating webapps directory"},
     {"action": "sudo", "params":
-     "chown -R %(SERVER_USERNAME)s: %(APPS_DIR)s"},
+        "chown -R %(SERVER_USERNAME)s: %(APPS_DIR)s"},
 
     # git setup
     {"action": "run", "params": "git config --global user.name '%(GIT_USERNAME)s'",
      "message": "Configuring git"},
     {"action": "run", "params":
-     "git config --global user.email '%(ADMIN_EMAIL)s'"},
+        "git config --global user.email '%(ADMIN_EMAIL)s'"},
     {"action": "put", "params": {"file": "%(GITHUB_DEPLOY_KEY_PATH)s",
                                  "destination": "/home/%(SERVER_USERNAME)s/.ssh/%(GITHUB_DEPLOY_KEY_NAME)s"}},
     {"action": "run", "params":
-     "chmod 600 /home/%(SERVER_USERNAME)s/.ssh/%(GITHUB_DEPLOY_KEY_NAME)s"},
+        "chmod 600 /home/%(SERVER_USERNAME)s/.ssh/%(GITHUB_DEPLOY_KEY_NAME)s"},
     {"action": "run",
      "params": """echo 'IdentityFile /home/%(SERVER_USERNAME)s/.ssh/%(GITHUB_DEPLOY_KEY_NAME)s' >> /home/%(SERVER_USERNAME)s/.ssh/config"""},
     {"action": "run", "params":
-     "ssh-keyscan github.com >> /home/%(SERVER_USERNAME)s/.ssh/known_hosts"},
+        "ssh-keyscan github.com >> /home/%(SERVER_USERNAME)s/.ssh/known_hosts"},
 
     # Create virtualevn
     {"action": "run", "params": "mkvirtualenv --no-site-packages %(PROJECT_NAME)s",
@@ -91,9 +91,9 @@ create_recipe_production = [
 
     # install imaging tools
     {"action": "sudo", "params":
-     "apt-get install -qq libjpeg62 libjpeg62-dev zlib1g-dev"},
+        "apt-get install -qq libjpeg62 libjpeg62-dev zlib1g-dev"},
     {"action": "sudo", "params":
-     "sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib"},
+        "sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib"},
 
     # install django in virtual env
     {"action": "virtualenv", "params": "git clone -b %(GITHUB_BRANCH)s %(GITHUB_REPO)s",
@@ -114,31 +114,31 @@ create_recipe_production = [
     {"action": "run",
      "params": "cat /home/%(SERVER_USERNAME)s/my.supervisord.conf >> /home/%(SERVER_USERNAME)s/supervisord.conf"},
     {"action": "run", "params":
-     "rm /home/%(SERVER_USERNAME)s/my.supervisord.conf"},
+        "rm /home/%(SERVER_USERNAME)s/my.supervisord.conf"},
     {"action": "sudo", "params":
-     "mv /home/%(SERVER_USERNAME)s/supervisord.conf /etc/supervisord.conf"},
+        "mv /home/%(SERVER_USERNAME)s/supervisord.conf /etc/supervisord.conf"},
     #{"action":"sudo", "params":"supervisord"},
     {"action": "put", "params": {"file": "%(FABULOUS_PATH)s/recipes/templates/supervisord-init",
                                  "destination": "/home/%(SERVER_USERNAME)s/supervisord-init"}},
     {"action": "sudo", "params":
-     "mv /home/%(SERVER_USERNAME)s/supervisord-init /etc/init.d/supervisord"},
+        "mv /home/%(SERVER_USERNAME)s/supervisord-init /etc/init.d/supervisord"},
     {"action": "sudo", "params": "chmod +x /etc/init.d/supervisord"},
     {"action": "sudo", "params": "update-rc.d supervisord defaults"},
     {"action": "sudo", "params": "sudo /etc/init.d/supervisord start"},
     {"action": "sudo", "params":
-     "sudo supervisorctl restart %(PROJECT_NAME)s"},
+        "sudo supervisorctl restart %(PROJECT_NAME)s"},
     # Install and setup celery
     {"action": "virtualenv", "params": "pip install celery django-celery",
      "message": "Install celery requirements"},
     {"action": "put_template", "params": {"template": "%(FABULOUS_PATH)s/recipes/templates/celeryd.conf",
                                           "destination": "/home/%(SERVER_USERNAME)s/celeryd.conf"}},
     {"action": "sudo", "params":
-     "cat /home/%(SERVER_USERNAME)s/celeryd.conf >> /etc/supervisord.conf"},
+        "cat /home/%(SERVER_USERNAME)s/celeryd.conf >> /etc/supervisord.conf"},
 
     {"action": "put_template", "params": {"template": "%(FABULOUS_PATH)s/recipes/templates/celerybeat.conf",
                                           "destination": "/home/%(SERVER_USERNAME)s/celerybeat.conf"}},
     {"action": "sudo", "params":
-     "cat /home/%(SERVER_USERNAME)s/celerybeat.conf >> /etc/supervisord.conf"},
+        "cat /home/%(SERVER_USERNAME)s/celerybeat.conf >> /etc/supervisord.conf"},
     {"action": "sudo", "params": "sudo supervisorctl update"},
     {"action": "sudo", "params": "sudo supervisorctl restart celery"},
     {"action": "sudo", "params": "sudo supervisorctl restart celerybeat"},
@@ -154,25 +154,25 @@ deploy_recipe_production = [
     {"action": "run", "params": "whoami"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git checkout %(GITBUH_STAGING_BRANCH)s"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git checkout %(GITBUH_STAGING_BRANCH)s"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git pull"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git pull"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git checkout %(GITHUB_BRANCH)s"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git checkout %(GITHUB_BRANCH)s"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git merge %(GITBUH_STAGING_BRANCH)s"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git merge %(GITBUH_STAGING_BRANCH)s"},
 
     {"action": "run", "params":
-    "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git tag -a %(TAG)s -m '%(TAG_MESSAGE)s'"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git tag -a %(TAG)s -m '%(TAG_MESSAGE)s'"},
 
     {"action": "run", "params":
         "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git push origin %(TAG)s"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git push"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git push"},
 
     {"action": "virtualenv", "params": "pip install -r %(PROJECT_NAME)s/requirements.txt",
      "message": "Install project requirements"},
@@ -201,11 +201,18 @@ create_recipe_staging = [
      "message": "Updating apt-get"},
 
     # List of APT packages to install
+    # for staging install rabbitmq on same server
     {"action": "apt",
      "params": ["nginx", "memcached", "git", "python-pip", "python-virtualenv", "postgresql-client",
-                "python-setuptools", "python-dev", "build-essential", "libpq-dev"],
+                "python-setuptools", "python-dev", "build-essential", "libpq-dev", "rabbitmq-server",
+                "postgresql"],
      "message": "Installing apt-get packages"},
+    # configure postgresql
+    {"action": "sudo", "params":
+        "sudo -u postgres psql -c \"create user stg_agent with password 'test';\""},
 
+    {"action": "sudo", "params":
+        "sudo -u postgres createdb -O stg_agent dlap"},
     # List of pypi packages to install
     {"action": "pip", "params": ["virtualenvwrapper", "supervisor"],
      "message": "Installing pip packages"},
@@ -215,34 +222,34 @@ create_recipe_staging = [
                                  "destination": "/home/%(SERVER_USERNAME)s/nginx.conf"},
      "message": "Configuring nginx"},
     {"action": "sudo", "params":
-     "mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old"},
+        "mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.old"},
     {"action": "sudo", "params":
-     "mv /home/%(SERVER_USERNAME)s/nginx.conf /etc/nginx/nginx.conf"},
+        "mv /home/%(SERVER_USERNAME)s/nginx.conf /etc/nginx/nginx.conf"},
     {"action": "sudo", "params": "chown root:root /etc/nginx/nginx.conf"},
     {"action": "put_template", "params": {"template": "%(FABULOUS_PATH)s/recipes/templates/nginx-app-proxy",
                                           "destination": "/home/%(SERVER_USERNAME)s/%(PROJECT_NAME)s"}},
     {"action": "sudo", "params":
-     "rm -rf /etc/nginx/sites-enabled/default"},
+        "rm -rf /etc/nginx/sites-enabled/default"},
     {"action": "sudo",
      "params": "mv /home/%(SERVER_USERNAME)s/%(PROJECT_NAME)s /etc/nginx/sites-available/%(PROJECT_NAME)s"},
     {"action": "sudo",
      "params": "ln -s /etc/nginx/sites-available/%(PROJECT_NAME)s /etc/nginx/sites-enabled/%(PROJECT_NAME)s"},
     {"action": "sudo", "params":
-     "chown root:root /etc/nginx/sites-available/%(PROJECT_NAME)s"},
+        "chown root:root /etc/nginx/sites-available/%(PROJECT_NAME)s"},
     {"action": "sudo", "params": "/etc/init.d/nginx restart",
      "message": "Restarting nginx"},
 
     # virtualenvwrapper
     {"action": "sudo", "params":
-     "mkdir %(VIRTUALENV_DIR)s", "message": "Configuring virtualenvwrapper"},
+        "mkdir %(VIRTUALENV_DIR)s", "message": "Configuring virtualenvwrapper"},
     {"action": "sudo", "params":
-     "chown -R %(SERVER_USERNAME)s: %(VIRTUALENV_DIR)s"},
+        "chown -R %(SERVER_USERNAME)s: %(VIRTUALENV_DIR)s"},
     {"action": "run", "params":
-     "echo 'export WORKON_HOME=%(VIRTUALENV_DIR)s' >> /home/%(SERVER_USERNAME)s/.profile"},
+        "echo 'export WORKON_HOME=%(VIRTUALENV_DIR)s' >> /home/%(SERVER_USERNAME)s/.profile"},
     {"action": "run",
      "params": "echo 'source /usr/local/bin/virtualenvwrapper.sh' >> /home/%(SERVER_USERNAME)s/.profile"},
     {"action": "run", "params":
-     "source /home/%(SERVER_USERNAME)s/.profile"},
+        "source /home/%(SERVER_USERNAME)s/.profile"},
 
     # webapps alias
     {"action": "run", "params": """echo "alias webapps='cd %(APPS_DIR)s'" >> /home/%(SERVER_USERNAME)s/.profile""",
@@ -250,23 +257,23 @@ create_recipe_staging = [
 
     # webapps dir
     {"action": "sudo", "params":
-     "mkdir %(APPS_DIR)s", "message": "Creating webapps directory"},
+        "mkdir %(APPS_DIR)s", "message": "Creating webapps directory"},
     {"action": "sudo", "params":
-     "chown -R %(SERVER_USERNAME)s: %(APPS_DIR)s"},
+        "chown -R %(SERVER_USERNAME)s: %(APPS_DIR)s"},
 
     # git setup
     {"action": "run", "params": "git config --global user.name '%(GIT_USERNAME)s'",
      "message": "Configuring git"},
     {"action": "run", "params":
-     "git config --global user.email '%(ADMIN_EMAIL)s'"},
+        "git config --global user.email '%(ADMIN_EMAIL)s'"},
     {"action": "put", "params": {"file": "%(GITHUB_DEPLOY_KEY_PATH)s",
                                  "destination": "/home/%(SERVER_USERNAME)s/.ssh/%(GITHUB_DEPLOY_KEY_NAME)s"}},
     {"action": "run", "params":
-     "chmod 600 /home/%(SERVER_USERNAME)s/.ssh/%(GITHUB_DEPLOY_KEY_NAME)s"},
+        "chmod 600 /home/%(SERVER_USERNAME)s/.ssh/%(GITHUB_DEPLOY_KEY_NAME)s"},
     {"action": "run",
      "params": """echo 'IdentityFile /home/%(SERVER_USERNAME)s/.ssh/%(GITHUB_DEPLOY_KEY_NAME)s' >> /home/%(SERVER_USERNAME)s/.ssh/config"""},
     {"action": "run", "params":
-     "ssh-keyscan github.com >> /home/%(SERVER_USERNAME)s/.ssh/known_hosts"},
+        "ssh-keyscan github.com >> /home/%(SERVER_USERNAME)s/.ssh/known_hosts"},
 
     # Create virtualevn
     {"action": "run", "params": "mkvirtualenv --no-site-packages %(PROJECT_NAME)s",
@@ -274,9 +281,9 @@ create_recipe_staging = [
 
     # install imaging tools
     {"action": "sudo", "params":
-     "apt-get install -qq libjpeg62 libjpeg62-dev zlib1g-dev"},
+        "apt-get install -qq libjpeg62 libjpeg62-dev zlib1g-dev"},
     {"action": "sudo", "params":
-     "sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib"},
+        "sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib"},
 
     # install django in virtual env
     {"action": "virtualenv", "params": "git clone -b %(GITBUH_STAGING_BRANCH)s %(GITHUB_REPO)s",
@@ -297,31 +304,31 @@ create_recipe_staging = [
     {"action": "run",
      "params": "cat /home/%(SERVER_USERNAME)s/my.supervisord.conf >> /home/%(SERVER_USERNAME)s/supervisord.conf"},
     {"action": "run", "params":
-     "rm /home/%(SERVER_USERNAME)s/my.supervisord.conf"},
+        "rm /home/%(SERVER_USERNAME)s/my.supervisord.conf"},
     {"action": "sudo", "params":
-     "mv /home/%(SERVER_USERNAME)s/supervisord.conf /etc/supervisord.conf"},
+        "mv /home/%(SERVER_USERNAME)s/supervisord.conf /etc/supervisord.conf"},
     #{"action":"sudo", "params":"supervisord"},
     {"action": "put", "params": {"file": "%(FABULOUS_PATH)s/recipes/templates/supervisord-init",
                                  "destination": "/home/%(SERVER_USERNAME)s/supervisord-init"}},
     {"action": "sudo", "params":
-     "mv /home/%(SERVER_USERNAME)s/supervisord-init /etc/init.d/supervisord"},
+        "mv /home/%(SERVER_USERNAME)s/supervisord-init /etc/init.d/supervisord"},
     {"action": "sudo", "params": "chmod +x /etc/init.d/supervisord"},
     {"action": "sudo", "params": "update-rc.d supervisord defaults"},
     {"action": "sudo", "params": "sudo /etc/init.d/supervisord start"},
     {"action": "sudo", "params":
-     "sudo supervisorctl restart %(PROJECT_NAME)s"},
+        "sudo supervisorctl restart %(PROJECT_NAME)s"},
     # Install and setup celery
     {"action": "virtualenv", "params": "pip install celery django-celery",
      "message": "Install celery requirements"},
     {"action": "put_template", "params": {"template": "%(FABULOUS_PATH)s/recipes/templates/celeryd.conf",
                                           "destination": "/home/%(SERVER_USERNAME)s/celeryd.conf"}},
     {"action": "sudo", "params":
-     "cat /home/%(SERVER_USERNAME)s/celeryd.conf >> /etc/supervisord.conf"},
+        "cat /home/%(SERVER_USERNAME)s/celeryd.conf >> /etc/supervisord.conf"},
 
     {"action": "put_template", "params": {"template": "%(FABULOUS_PATH)s/recipes/templates/celerybeat.conf",
                                           "destination": "/home/%(SERVER_USERNAME)s/celerybeat.conf"}},
     {"action": "sudo", "params":
-     "cat /home/%(SERVER_USERNAME)s/celerybeat.conf >> /etc/supervisord.conf"},
+        "cat /home/%(SERVER_USERNAME)s/celerybeat.conf >> /etc/supervisord.conf"},
     {"action": "sudo", "params": "sudo supervisorctl update"},
     {"action": "sudo", "params": "sudo supervisorctl restart celery"},
     {"action": "sudo", "params": "sudo supervisorctl restart celerybeat"},
@@ -335,19 +342,19 @@ deploy_recipe_staging = [
     {"action": "run", "params": "whoami"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git checkout %(GITBUH_DEV_BRANCH)s"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git checkout %(GITBUH_DEV_BRANCH)s"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git pull"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git pull"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git checkout %(GITBUH_STAGING_BRANCH)s"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git checkout %(GITBUH_STAGING_BRANCH)s"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git merge %(GITBUH_DEV_BRANCH)s"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git merge %(GITBUH_DEV_BRANCH)s"},
 
     {"action": "run", "params":
-     "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git push"},
+        "cd %(APPS_DIR)s/%(PROJECT_NAME)s; git push"},
 
     {"action": "virtualenv", "params": "pip install -r %(PROJECT_NAME)s/requirements.txt",
      "message": "Install project requirements"},
@@ -365,14 +372,14 @@ deploy_recipe_staging = [
 
 restart_services = \
     {"nginx": [{"action": "sudo", "params": "sudo service nginx restart",
-               "message": "Restarting nginx"}],
+                "message": "Restarting nginx"}],
      "gunicorn": [{"action": "sudo", "params": "sudo supervisorctl restart %(PROJECT_NAME)s",
                    "message": "Restarting gunicorn"}],
      "celery": [{"action": "sudo", "params": "sudo supervisorctl restart celery",
                  "message": "Restarting celery"},
                 {"action": "sudo", "params": "sudo supervisorctl restart celerybeat",
                  "message": "Restarting celerybeat"}]
-     }
+    }
 
 log_services = \
     {"nginx": [{"action": "sudo", "params": "sudo tail /tmp/nginx.access.log",
@@ -383,4 +390,4 @@ log_services = \
                  "message": "Celery logs"}],
      "celerybeat": [{"action": "sudo", "params": "sudo tail /var/log/celerybeat.log",
                      "message": "Celerybeat logs"}]
-     }
+    }
